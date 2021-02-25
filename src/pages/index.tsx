03ab9249254ba1820cd35d6e471e24fb70f85464
aa <aaaa>
2021-02-25 15:08:08 -0300

    NLW 4 - First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { CompleteChallengs } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
+import Head from "next/head"
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}> 
+      <Head>
+        <title>Início | Moveit</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompleteChallengs />
+          <Countdown />
+        </div>
+        <div>
+          <ChallengeBox />
+        </div>
+      </section>
+    </div>
+  )
+}
