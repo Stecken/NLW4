e04cf139db46eb567a9a56b891e56d3a24863885
aa <aaaa>
2021-02-26 14:14:05 -0300

    Move It v2

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/stecken.png" alt="Yuri Martins" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Yuri Martins</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
